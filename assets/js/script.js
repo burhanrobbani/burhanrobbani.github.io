d49bade55dd02bbe6f477fc2a4789e3ce2f5dc27
burhanrobbani <82148400+burhanrobbani@users.noreply.github.com>
2022-09-29 21:55:38 +0700

    perubahan nama

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,33 @@ menuBar.addEventListener('click', function name(params) {
 })
 
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 //selecting all required elements
 const filterItem = document.querySelector(".items");
 const filterImg = document.querySelectorAll(".gallery .image");
